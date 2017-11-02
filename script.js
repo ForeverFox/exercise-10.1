0824d67bc468213d079b14e437faa711ed07f95d
Forever Fox <cogden@mcad.edu>
2017-11-02 02:46:05 +0200

    Edited JS after watching second part of video

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("document").ready(function() {
-	$("h2[data-type=vitamin]").css("backgroundColor", "darkgreen");
-	$("h2[data-type=mineralwater]").css("backgroundColor", "blue");
-	$("h2[data-type=proteinbar]").css("backgroundColor", "purple");
+	$("#products h2.product-name[data-type='vitamin']").css("backgroundColor", "darkgreen");
+	$("#products h2.product-name[data-type='mineralwater']").css("backgroundColor", "blue");
+	$("#products h2.product-name[data-type='proteinbar']").css("backgroundColor", "purple");
 });
