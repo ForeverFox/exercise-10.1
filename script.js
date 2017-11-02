f211fc5fc68a170cf9b61733aa8a1b42bbf46c64
Forever Fox <cogden@mcad.edu>
2017-11-02 02:34:07 +0200

    Initial Commit
    With my added JS file

---
+++ script.js
@@ -0,0 +1,5 @@
+$("document").ready(function() {
+	$("h2[data-type=vitamin]").css("backgroundColor", "darkgreen");
+	$("h2[data-type=mineralwater]").css("backgroundColor", "blue");
+	$("h2[data-type=proteinbar]").css("backgroundColor", "purple");
+});
